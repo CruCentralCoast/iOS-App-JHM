88f06d2536e747ef2d513d94986fe52b855d9887
petegodkin <petegodkin@gmail.com>
2015-11-18 12:32:14 -0800

    added code complete DBClient jk

---
+++ Cru/CruDBClient.swift
@@ -0,0 +1,21 @@
+//
+//  CruDBClient.swift
+//  Cru
+//
+//  Created by Peter Godkin on 11/18/15.
+//  Copyright © 2015 Jamaican Hopscotch Mafia. All rights reserved.
+//
+
+import Foundation
+
+class CruDBClient {
+    var something : String
+    
+    init() {
+        self.something = "Maxxx Crane"
+    }
+    
+    func getSomething() -> String {
+        return self.something
+    }
+}
