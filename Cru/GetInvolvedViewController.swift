a817ceb949d1df5f235edc19be57b00fc30bd692
maxcrane <Max@pcp135399pcs.wireless.calpoly.edu>
2015-11-19 14:41:22 -0800

    added swreveal

---
+++ Cru/GetInvolvedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GetInvolvedViewController.swift
+//  Cru
+//
+//  Created by Max Crane on 11/17/15.
+//  Copyright © 2015 Jamaican Hopscotch Mafia. All rights reserved.
+//
+
+import UIKit
+
+class GetInvolvedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
