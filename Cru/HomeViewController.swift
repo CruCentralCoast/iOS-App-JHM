5b937d2be673e4e7c36c8c8e4a369298822e3293
maxcrane <Maxlccrane@gmail.com>
2015-11-23 21:07:14 -0800

    Fixed simulator loading to black issues. Created all the screen templates for the app.

--- Cru/HomeViewController.swift
+++ Cru/HomeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var menuButton: UIBarButtonItem!
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
