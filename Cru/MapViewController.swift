7e3e9a991f1741991b4dd7aaed1ee837ff3eae04
maxcrane <Maxlccrane@gmail.com>
2015-11-30 21:57:47 -0800

    added map but still need to make it dynamically load the address specified from the ridesharing screen.

---
+++ Cru/MapViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MapViewController.swift
+//  Cru
+//
+//  Created by Max Crane on 11/30/15.
+//  Copyright © 2015 Jamaican Hopscotch Mafia. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMap()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupMap(){
+        var initialLocation = CLLocation()
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "1128 Peach San Luis Obispo"
+        request.region = mapView.region
+
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { (response, error) in
+            guard let response = response else {
+                print("Search error: \(error)")
+                return
+            }
+            
+            for item in response.mapItems {
+                
+                initialLocation = item.placemark.location!
+                print(item)
+            }
+        }
+        
+        initialLocation = CLLocation(latitude: 35.28564100, longitude: -120.66179700)
+        centerMapOnLocation(initialLocation)
+    }
+
+    
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
