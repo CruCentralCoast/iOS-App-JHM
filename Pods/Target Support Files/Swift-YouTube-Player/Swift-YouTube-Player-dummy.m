f147761edb2f67f56acd8ecdca29ca12fb96b662
Deniz Tumer <e.tumer.deniz@gmail.com>
2015-12-17 23:59:02 -0800

    Added cocoa pods integration and added one Pod entitled "Swift-YouTube-Player" that is a wrapper class for youtube embedded videos

---
+++ Pods/Target Support Files/Swift-YouTube-Player/Swift-YouTube-Player-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Swift_YouTube_Player : NSObject
+@end
+@implementation PodsDummy_Swift_YouTube_Player
+@end
---
+++ Pods/Target Support Files/Swift-YouTube-Player/Swift-YouTube-Player-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Swift_YouTube_PlayerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Swift_YouTube_PlayerVersionString[];
+
