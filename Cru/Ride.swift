6d01cb1440394a47fbd1dddc02dd508b1cd578c4
qqtip <tranmqt@gmail.com>
2016-01-21 14:49:20 -0800

    added Ride class

---
+++ Cru/Ride.swift
@@ -0,0 +1,34 @@
+//
+//  Ride.swift
+//  Cru
+//
+//  Created by Quan Tran on 1/21/16.
+//  Copyright © 2016 Jamaican Hopscotch Mafia. All rights reserved.
+//
+
+import Foundation
+
+class Ride {
+    
+    //MARK: Properties
+
+    var driver:User
+    var passengers:[User]?
+    var seats:Int
+    var event:Event
+    var departureTime:String
+    var departureAddress:String
+    var roundTrip:Bool
+    
+    
+    //MARK: Initialization
+
+    init?(driver:User, seats:Int, event:Event, departureTime:String, departureAddress:String, roundTrip:Bool) {
+        self.driver = driver
+        self.seats = seats
+        self.event = event
+        self.departureTime = departureTime
+        self.departureAddress = departureAddress
+        self.roundTrip = roundTrip
+    }
+}
