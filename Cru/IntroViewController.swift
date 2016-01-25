af4124f1b0f9442900e975c5dc12c6376a2b28f6
maxcrane <Maxlccrane@gmail.com>
2016-01-24 16:47:45 -0800

    Cleaned up ministry selection modal.

--- Cru/IntroViewController.swift
+++ Cru/IntroViewController.swift
@@ -191,26 +191,14 @@ class IntroViewController: UIViewController {
         
         if(introModals[nextNdx] == ministriesModal){
             embeddedMinistryViewController.reloadCampuses()
-//            let navController = ministriesModal.window?.rootViewController as! UINavigationController
-//            print("There are \(navController.viewControllers.count) vcs")
-//            print("\(navController.viewControllers)")
-//            for viewController in navController.viewControllers{
-//                if(viewController.isKindOfClass(MinistryTableViewController)){
-//                    let ministryTVC = viewController as! MinistryTableViewController
-//                    ministryTVC.reloadCampuses()    
-//                }
-//            }
-            
         }
         displayModal(introModals[nextNdx], fromModal: currentModal)
         
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
         if let vc = segue.destinationViewController as? MinistryTableViewController where segue.identifier == "ministrySegue" {
-            print("I happened") 
-            self.embeddedMinistryViewController = vc
+            embeddedMinistryViewController = vc
         }
     }
     
