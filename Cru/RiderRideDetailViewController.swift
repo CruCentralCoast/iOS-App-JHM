547deda035641dd25a93dd65d603544a215a78df
maxcrane <Max@pcp135257pcs.wireless.calpoly.edu>
2016-02-09 16:28:57 -0800

    Add MapView and clickable address to rider ride detail page. Consolidate time for ride into one UITextView.

--- Cru/RiderRideDetailViewController.swift
+++ Cru/RiderRideDetailViewController.swift
@@ -7,25 +7,27 @@
 //
 
 import UIKit
+import MapKit
 
 class RiderRideDetailViewController: UIViewController {
+    let roundTrip = "round-trip"
+    let roundTripDirection = "both"
+    let fromEvent = "from event"
+    let toEvent = "to event"
+    let driver = "driver"
+    let rider = "rider"
+    
     var event: Event?
     var ride: Ride?
     
-    
     @IBOutlet weak var eventTitle: UILabel!
-    
-    @IBOutlet weak var departureTime: UILabel!
-    @IBOutlet weak var departureMonth: UILabel!
-    @IBOutlet weak var departureDay: UILabel!
-    @IBOutlet weak var pickupAddress: UILabel!
-    
     @IBOutlet weak var direction: UILabel!
     @IBOutlet weak var directionIcon: UIImageView!
-    
     @IBOutlet weak var driverName: UILabel!
-    
+    @IBOutlet weak var date: UITextView!
     @IBOutlet weak var driverNumber: UITextView!
+    @IBOutlet weak var pickupMap: MKMapView!
+    @IBOutlet weak var address: UITextView!
     
     
     override func viewDidLoad() {
@@ -34,26 +36,76 @@ class RiderRideDetailViewController: UIViewController {
         navigationItem.title = "Ride Details"
         
         eventTitle.text = event!.name
-        departureTime.text = ride!.time
-        departureMonth.text = String(ride!.month)
-        departureDay.text = String(ride!.day)
+        date.text = String(ride!.month) + "   " + String(ride!.day) + "   " + ride!.time
         
-        pickupAddress.sizeToFit()
+        //TODO: get address from ride 
+        address.text = "1 Grand Ave. 93405 San Luis Obispo"
+        //address.sizeToFit()
+
+        setupMap()
         
-        //pickupAddress =
-        //direction = 
-        //directionIcon
         driverName.text = ride!.driverName
-        
         driverNumber.text = ride!.driverNumber
-        driverNumber.editable = false
-        driverNumber.dataDetectorTypes = UIDataDetectorTypes.PhoneNumber
         
-//        eventName.text = event!.name
-//        driverName.text = ride!.driverName + " is driving"
-//        numSeatLabel.text = "1/5 seats"  //ride!.seats + " seats are available"
         
-        // Do any additional setup after loading the view.
+        
+        if(ride!.direction == roundTripDirection){
+            directionIcon.image = UIImage(named: "twoway")
+            direction.text = roundTrip
+        }
+        else if(ride!.direction == "from"){
+            directionIcon.image = UIImage(named: "oneway")
+            direction.text = fromEvent
+
+            //mirrors arrow
+            directionIcon.transform = CGAffineTransformMakeScale(-1, 1)
+        }
+        else if (ride!.direction == "to"){
+            directionIcon.image = UIImage(named: "oneway")
+            direction.text = toEvent
+        }
+        
+    }
+    
+    func setupMap(){
+        
+        var initialLocation = CLLocation()
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = self.address.text//"1128 Peach San Luis Obispo"
+        
+        if(request.naturalLanguageQuery != nil){
+            request.naturalLanguageQuery = self.address.text
+        }
+        
+        request.region = pickupMap.region
+        
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { (response, error) in
+            guard let response = response else {
+                print("Search error: \(error)")
+                return
+            }
+            
+            for item in response.mapItems {
+                initialLocation = item.placemark.location!
+                let dropPin = MKPointAnnotation()
+                dropPin.coordinate = initialLocation.coordinate
+                dropPin.title = self.address.text
+                
+                
+                self.centerMapOnLocation(initialLocation)
+                self.pickupMap.addAnnotation(dropPin)
+            }
+        }
+    }
+    
+    
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        pickupMap.setRegion(coordinateRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
