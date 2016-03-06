7eac4fcb72fc7b52f0ccc2fd46ebd8f9785ae891
Peter Godkin <pete@Peters-MacBook-Pro.local>
2016-03-06 15:11:58 -0800

    Finished up login so you can logout and it will remember the username from your last login

---
+++ Cru/LogoutViewController.swift
@@ -0,0 +1,24 @@
+
+
+import UIKit
+
+class LogoutViewController: UIViewController {
+
+
+    @IBOutlet weak var loggedInLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        loggedInLabel.text = "You are alredy logged in as " + SubscriptionManager.loadString(Config.username)
+    }
+    
+    
+    
+    @IBAction func logoutPressed(sender: AnyObject) {
+        LoginUtils.logout()
+        if let navController = self.navigationController {
+            navController.popViewControllerAnimated(true)
+        }
+    }
+    
+}
