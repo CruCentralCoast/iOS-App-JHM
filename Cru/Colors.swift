d42fa4ea0efa8a47cd1ba199bd441d0740383481
Peter Godkin <pete@Peters-MacBook-Pro.local>
2016-03-06 18:57:48 -0800

    Fixed issue JHM-100 where resource cards didn't display correctly

--- Cru/Colors.swift
+++ Cru/Colors.swift
@@ -11,4 +11,5 @@ import Foundation
 class Colors {
     static let green = UIColor(red: 0.0, green: 0.6, blue: 0.2, alpha: 1.0)
     static let darkerGreen = UIColor(red: 0.0, green: 0.4, blue: 0.13, alpha: 1.0)
+    static let googleGray = UIColor(red: 0.871, green: 0.871, blue: 0.871, alpha: 1.0)
 }
--- Cru/ResourcesViewController.swift
+++ Cru/ResourcesViewController.swift
@@ -37,7 +37,7 @@ class ResourcesViewController: UIViewController, UITableViewDelegate, UITableVie
         
         //tableView.reloadData()
         
-        tableView.backgroundColor = UIColor(red: 0.871, green: 0.871, blue: 0.871, alpha: 1.0)
+        tableView.backgroundColor = Colors.googleGray
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 50
         
@@ -51,17 +51,47 @@ class ResourcesViewController: UIViewController, UITableViewDelegate, UITableVie
     //Get resources from database
     func insertResource(dict : NSDictionary) {
         
-        resources.insert(Resource(dict: dict)!, atIndex: 0)
+        let resource = Resource(dict: dict)!
         
-        self.tableView.insertRowsAtIndexPaths([NSIndexPath(forItem: 0, inSection: 0)], withRowAnimation: .Automatic)
+        resources.insert(resource, atIndex: 0)
         
+        let url = NSURL.init(string: resource.url)
+        
+        Card.getFromUrl(url) {(card: Card?, error: NSError?) in
+            
+            if (error != nil) {
+                print("Error getting card from url: \(error?.localizedDescription)")
+            } else if(card != nil) {
+                
+                var cardView : CardView! = nil
+                
+                if (resource.type == "article") {
+                    cardView = CardView.createCardView(card!, layout: .ArticleCardTall)!
+                } else if (resource.type == "video") {
+                    cardView = CardView.createCardView(card!, layout: .SummaryCardTall)!
+                } else if (resource.type == "audio") {
+                    cardView = CardView.createCardView(card!, layout: .VideoCardShort)!
+                }
+                
+                if (cardView != nil) {
+                    cardView.delegate = self
+                    self.resources.insert(Resource(dict: dict)!, atIndex: 0)
+                    self.cardViews.insert(cardView, atIndex: 0)
+                    self.tableView.insertRowsAtIndexPaths([NSIndexPath(forItem: 0, inSection: 0)], withRowAnimation: .Automatic)
+                }
+            } else {
+                print("\nERROR: Card view not created\n")
+            }
+        }
     }
+    
+    
     
     //Make and format every card view
     func formatResources() {
         
         //tableView.endUpdates()
-        tableView.reloadData()
+        //		tableView.reloadData()
         
         
     }
@@ -76,161 +106,48 @@ class ResourcesViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //print("\nNum card views in the thing: \(cardViews.count)\n")
         print("\nNum Resources: \(resources.count)\n")
-        return resources.count
+        return cardViews.count
     }
     
     //Configures each cell in the table view as a card and sets the UI elements to match with the Resource data
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cellIdentifier = "CardTableViewCell"
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! CardTableViewCell
-        //let cardView = cardViews[indexPath.row]
+        let cardView = cardViews[indexPath.row]
         
-        let res = resources[indexPath.row]
-       
-        let url = NSURL.init(string: res.url)
+        cell.contentView.addSubview(cardView)
+        cell.contentView.backgroundColor = Colors.googleGray
         
+        self.constrainView(cardView, row: indexPath.row)
         
-        Card.getFromUrl(url) {
-            (card: Card?, error: NSError?) in
-                
-                if(card != nil) {
-                    
-                    //Format card as an article
-                    if (res.type == "article") {
-                        //NEW CODE STARTS HERE
-                        /*let articleURL = url
-                        let articleData:NSMutableDictionary = NSMutableDictionary()
-                        let articleBaseData:NSMutableDictionary = NSMutableDictionary()
-                        articleData["htmlContent"] = "<div>Some Content</div>"
-                        articleData["publicationDate"] = NSNumber(longLong: 1429063354000)
-                        let articleMedia:NSMutableDictionary = NSMutableDictionary()
-                        articleMedia["imageUrl"] =  "http://i2.cdn.turner.com/money/dam/assets/150414182846-high-there-image-1024x576.jpeg"
-                        articleMedia["type"] = "image"
-                        articleData["media"] = articleMedia
-                        articleBaseData["article"] = articleData
-                        
-                        articleCard = ArticleCard(title: "The surprising backstory of Tinder for pot smokers", abstractContent: "That's what co-founder Todd Mitchem hopes to create with High There -- \"a social connection app for cannabis consumers.\" Users can look for dates or simply other like-minded people. But for those used to dating apps like Tinder or OkCupid, the questions will probably be a bit unfamiliar.", url: articleURL, creator: cnn, data: articleBaseData)
-                        */
-                        
-                        //let aCard = card as! ArticleCard
-                        //print("\nHTML: \(aCard.html)\n")
-                        
-                        /*
-                        let dataArray = aCard.media!
-                        print("Data items count: \(dataArray.count)\n")
-
-                        for (key, value) in dataArray {
-                            print("Property: \(key as! String)\n")
-                            if value is NSNull {
-                                print("Value: Null\n")
-                            }
-                            else {
-                                print("Value: \(value as! String)\n")
-                            }
-                        }*/
-                        
-                        
-                        //NEW CODE ENDS HERE
-                        
-                        
-                        let articleCardView = CardView.createCardView(card!, layout: .ArticleCardTall)
-                        if(articleCardView != nil) {
-                            articleCardView!.delegate = self
-                            
-                            print("Added article card\n")
-                            
-                            cell.contentView.addSubview(articleCardView!)
-                            cell.contentView.backgroundColor = UIColor(red: 0.871, green: 0.871, blue: 0.871, alpha: 1.0)
-                            articleCardView!.translatesAutoresizingMaskIntoConstraints = false
-                            articleCardView!.horizontallyCenterToSuperView(0)
-                            
-                            if(indexPath.row == 0) {
-                                articleCardView!.constrainTopToSuperView(15)
-                            }
-                            else {
-                                articleCardView!.constrainTopToSuperView(7)
-                            }
-                            
-                            if(indexPath.row == self.resources.count-1) {
-                                articleCardView!.constrainBottomToSuperView(15)
-                            }
-                            else {
-                                articleCardView!.constrainBottomToSuperView(8)
-                            }
-                            
-                            articleCardView!.constrainRightToSuperView(15)
-                            articleCardView!.constrainLeftToSuperView(15)
-                            
-                        }
-                        
-                    }
-                        
-                        //Format card as an video
-                    else if (res.type == "video") {
-                        let videoCardView = CardView.createCardView(card!, layout: .SummaryCardTall)
-                        if(videoCardView != nil) {
-                            videoCardView!.delegate = self
-                            //self.cardViews.append(videoCardView!)
-                            print("Added video card\n")
-                            
-                            cell.contentView.addSubview(videoCardView!)
-                            cell.contentView.backgroundColor = UIColor(red: 0.871, green: 0.871, blue: 0.871, alpha: 1.0)
-                            videoCardView!.translatesAutoresizingMaskIntoConstraints = false
-                            videoCardView!.horizontallyCenterToSuperView(0)
-                            
-                            if(indexPath.row == 0) {
-                                videoCardView!.constrainTopToSuperView(15)
-                            }
-                            else {
-                                videoCardView!.constrainTopToSuperView(7)
-                            }
-                            
-                            if(indexPath.row == self.resources.count-1) {
-                                videoCardView!.constrainBottomToSuperView(15)
-                            }
-                            else {
-                                videoCardView!.constrainBottomToSuperView(8)
-                            }
-                            
-                            //videoCardView!.constrainTopToSuperView(15)
-                            //videoCardView!.constrainBottomToSuperView(7)
-                            videoCardView!.constrainRightToSuperView(15)
-                            videoCardView!.constrainLeftToSuperView(15)
-                            
-                        }
-                        
-                    }
-                        
-                        //Format card as an audio file
-                    else if (res.type == "audio") {
-                        
-                        
-                        //START OLD CO
-                        let audioCardView = CardView.createCardView(card!, layout: .VideoCardShort)
-                        if(audioCardView != nil) {
-                            audioCardView!.delegate = self
-                            //self.cardViews.append(audioCardView!)
-                            print("Added audio card\n")
-                            
-                            cell.contentView.addSubview(audioCardView!)
-                            cell.contentView.backgroundColor = UIColor(red: 0.871, green: 0.871, blue: 0.871, alpha: 1.0)
-                            audioCardView!.translatesAutoresizingMaskIntoConstraints = false
-                            audioCardView!.horizontallyCenterToSuperView(0)
-                            audioCardView!.constrainTopToSuperView(10)
-                            audioCardView!.constrainBottomToSuperView(10)
-                            audioCardView!.constrainRightToSuperView(10)
-                            audioCardView!.constrainLeftToSuperView(10)
-                        }
-                        
-                    }
-                }
-                else {
-                    print("\nERROR: Card view not created\n")
-                }
-            }
         
         return cell
     }
+    
+    private func constrainView(cardView: CardView, row: Int) {
+        cardView.delegate = self
+        cardView.translatesAutoresizingMaskIntoConstraints = false
+        cardView.horizontallyCenterToSuperView(0)
+        
+        if(row == 0) {
+            cardView.constrainTopToSuperView(15)
+        }
+        else {
+            cardView.constrainTopToSuperView(7)
+        }
+        
+        if(row == self.resources.count-1) {
+            cardView.constrainBottomToSuperView(15)
+        }
+        else {
+            cardView.constrainBottomToSuperView(8)
+        }
+        
+        cardView.constrainRightToSuperView(15)
+        cardView.constrainLeftToSuperView(15)
+        
+    }
+    
     override func viewDidAppear(animated: Bool) {
         /*for subview in self.tableView.subviews {
             if (NSStringFromClass(subview.dynamicType) == "UITableViewWrapperView")
