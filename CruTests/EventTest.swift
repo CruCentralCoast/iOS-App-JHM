1a469e8b3ffa953899bd330899d9b19c49851a56
Deniz Tumer <e.tumer.deniz@gmail.com>
2016-03-10 04:20:35 -0800

    Added test file for Event.swift and added folders for other test files to be stored

---
+++ CruTests/EventTest.swift
@@ -0,0 +1,25 @@
+//
+//  EventTest.swift
+//  Cru
+//
+//  Created by Deniz Tumer on 3/10/16.
+//  Copyright © 2016 Jamaican Hopscotch Mafia. All rights reserved.
+//
+
+import XCTest
+
+class EventTest: XCTestCase {
+    let eventDict1 = ["_id": "blah", "url": "www.whocares.com", "description": "This is a description", "name": "Event Name", "notificationDate": "2015-03-05T16:09:41.000Z", "parentMinistry": "1", "notifications": ["hello", "there"], "parentMinistries": ["min1", "min2"], "ridesharingEnabled": true, "endDate": "2015-03-05T16:09:41.000Z", "startDate": "2015-03-05T16:09:41.000Z"]
+    
+    func testEventCreation1() {
+        let event = Event()!
+        
+        XCTAssertEqual(event.id, "")
+    }
+    
+    func testEventCreationDict() {
+        let event = Event(dict: eventDict1)!
+        
+        XCTAssertEqual(event.id, "blah")
+    }
+}
