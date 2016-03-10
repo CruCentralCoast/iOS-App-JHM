db9c097bfcd0f60e71a67946bbfd71ccda9c4914
Erica Solum <ericasolum@pcp079319pcs.wireless.calpoly.edu>
2016-03-10 12:36:39 -0800

    Changed the Cancel Ride dialog.

--- Cru/Cancler.swift
+++ Cru/Cancler.swift
@@ -11,10 +11,10 @@ import UIKit
 class Cancler {
     
     static func confirmCancel(view: UIViewController, handler: (action: UIAlertAction)->()){
-        let cancelAlert = UIAlertController(title: "Are you sure you want to leave this ride?", message: "This action is permanent", preferredStyle: UIAlertControllerStyle.Alert)
+        let cancelAlert = UIAlertController(title: "Are you sure you want to cancel this ride?", message: "This action is permanent.", preferredStyle: UIAlertControllerStyle.Alert)
         
-        cancelAlert.addAction(UIAlertAction(title: "Confirm", style: .Destructive, handler: handler))
-        cancelAlert.addAction(UIAlertAction(title: "Cancel", style: .Default, handler: nil))
+        cancelAlert.addAction(UIAlertAction(title: "Cancel the Ride", style: .Destructive, handler: handler))
+        cancelAlert.addAction(UIAlertAction(title: "Just Kidding", style: .Default, handler: nil))
         view.presentViewController(cancelAlert, animated: true, completion: nil)
     }
     
