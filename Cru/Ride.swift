e8b175a08d71da99c2379cb0603db06bcf03fea1
maxcrane <Max@LC3.att.net>
2016-05-05 17:03:50 -0700

    major cleanup of ride class, particularly removed constants, and wrote function to record a ride as a dictionary

--- Cru/Ride.swift
+++ Cru/Ride.swift
@@ -8,86 +8,105 @@
 
 import Foundation
 
+
+struct RideKeys {
+    static let id = "_id"
+    static let direction = "direction"
+    static let driverName = "driverName"
+    static let driverNumber = "driverNumber"
+    static let event = "event"
+    static let gcm_id = "gcm_id"
+    static let gender = "gender" //int
+    static let radius = "radius" //int
+    static let seats = "seats" //int
+    static let v = "__v"
+    static let time = "time" //some format that ends in Z
+    static let location = "location" //dict
+    static let passengers = "passengers" //list
+}
+
+struct LocationKeys {
+    static let loc = "location"
+    static let postcode = "postcode"
+    static let street1 = "street1"
+    static let suburb = "suburb"
+    static let state = "state"
+    static let country = "country"
+}
+
 class Ride: Comparable, Equatable, TimeDetail {
-    var id: String
-    var direction: String
-    var seats: Int
-    var radius: Int
-    var gcmId: String
-    var driverNumber: String
-    var driverName: String
-    var eventId: String
-    var time: String
-    var passengers: [String]
-    var day = 3
-    var month = "JAN"
-    var monthNum = 1
-    var hour = 3
-    var minute = 3
-    var year = 2016
+    var id: String = ""
+    var direction: String = ""
+    var seats: Int = -1
+    var radius: Int = -1
+    var gcmId: String = ""
+    var driverNumber: String = ""
+    var driverName: String = ""
+    var eventId: String = ""
+    var time: String = ""
+    var passengers = [String]()
+    var day = -1
+    var month = ""
+    var monthNum = -1
+    var hour = -1
+    var minute = -1
+    var year = -1
     var date : NSDate?
     var postcode: String = ""
     var state: String = ""
     var suburb: String = ""
     var street: String = ""
+    var country: String = ""
     var gender: Int = 0
     
     
     init?(dict: NSDictionary){
-        id = "blah"
-        direction = "both"
-        seats = 3
-        radius = 1
-        gcmId = "blah"
-        driverNumber = "123456789"
-        driverName = "Max crane"
-        eventId = "563b11135e926d03001ac15c"
-        time = "5:00 pm"
-        passengers = [String]()
-        
-        
-        if (dict.objectForKey("location") != nil){
-            let loc = dict.objectForKey("location") as! NSDictionary
+
+        if (dict.objectForKey(LocationKeys.loc) != nil){
+            let loc = dict.objectForKey(LocationKeys.loc) as! NSDictionary
             
-            if let postcode = loc["postcode"] as? String{
-                //something
+            if (loc[LocationKeys.postcode] != nil){
+                postcode = loc[LocationKeys.postcode] as! String
             }
-            if (loc["state"] != nil){
-                state = loc["state"] as! String
+            if (loc[LocationKeys.state] != nil){
+                state = loc[LocationKeys.state] as! String
             }
-            if (loc["suburb"] != nil){
-                suburb = loc["suburb"] as! String
+            if (loc[LocationKeys.suburb] != nil){
+                suburb = loc[LocationKeys.suburb] as! String
             }
-            if (loc["street1"] != nil){
-                street = loc["street1"] as! String
+            if (loc[LocationKeys.street1] != nil){
+                street = loc[LocationKeys.street1] as! String
+            }
+            if loc[LocationKeys.country] != nil {
+                country = loc[LocationKeys.country] as! String
             }
         }
-        if (dict.objectForKey("_id") != nil){
-            id = dict.objectForKey("_id") as! String
+        if (dict.objectForKey(RideKeys.id) != nil){
+            id = dict.objectForKey(RideKeys.id) as! String
         }
-        if (dict.objectForKey("direction") != nil){
-            direction = dict.objectForKey("direction") as! String
+        if (dict.objectForKey(RideKeys.direction) != nil){
+            direction = dict.objectForKey(RideKeys.direction) as! String
         }
-        if (dict.objectForKey("seats") != nil){
-            seats = dict.objectForKey("seats") as! Int
+        if (dict.objectForKey(RideKeys.seats) != nil){
+            seats = dict.objectForKey(RideKeys.seats) as! Int
         }
-        if (dict.objectForKey("radius") != nil){
-            radius = dict.objectForKey("radius") as! Int
+        if (dict.objectForKey(RideKeys.radius) != nil){
+            radius = dict.objectForKey(RideKeys.radius) as! Int
         }
-        if (dict.objectForKey("gcm_id") != nil){
-            gcmId = dict.objectForKey("gcm_id") as! String
+        if (dict.objectForKey(RideKeys.gcm_id) != nil){
+            gcmId = dict.objectForKey(RideKeys.gcm_id) as! String
         }
-        if (dict.objectForKey("driverNumber") != nil){
-            driverNumber = dict.objectForKey("driverNumber") as! String
+        if (dict.objectForKey(RideKeys.driverNumber) != nil){
+            driverNumber = dict.objectForKey(RideKeys.driverNumber) as! String
         }
-        if (dict.objectForKey("driverName") != nil){
-            driverName = dict.objectForKey("driverName") as! String
+        if (dict.objectForKey(RideKeys.driverName) != nil){
+            driverName = dict.objectForKey(RideKeys.driverName) as! String
         }
-        if (dict.objectForKey("event") != nil){
-            eventId = dict.objectForKey("event") as! String
+        if (dict.objectForKey(RideKeys.event) != nil){
+            eventId = dict.objectForKey(RideKeys.event) as! String
         }
-        if (dict.objectForKey("time") != nil){
-            time = dict.objectForKey("time") as! String
+        if (dict.objectForKey(RideKeys.time) != nil){
+            time = dict.objectForKey(RideKeys.time) as! String
             self.date = GlobalUtils.dateFromString(time)
             
             let components = GlobalUtils.dateComponentsFromDate(GlobalUtils.dateFromString(time))!
@@ -104,8 +123,8 @@ class Ride: Comparable, Equatable, TimeDetail {
             self.month = months[monthNumber - 1].uppercaseString
             self.monthNum = monthNumber
         }
-        if (dict.objectForKey("passengers") != nil){
-            passengers = dict.objectForKey("passengers") as! [String]
+        if (dict.objectForKey(RideKeys.passengers) != nil){
+            passengers = dict.objectForKey(RideKeys.passengers) as! [String]
         }
 
     }
@@ -149,6 +168,40 @@ class Ride: Comparable, Equatable, TimeDetail {
     
     func seatsLeft()->Int{
         return self.seats - self.passengers.count
+    }
+    
+    func getRideAsDict()->[String:AnyObject]{
+        var map = [String:AnyObject]()
+        map[RideKeys.id] = self.id
+        map[RideKeys.direction] = self.direction
+        map[RideKeys.driverName] = self.driverName
+        map[RideKeys.driverNumber] = self.driverNumber
+        map[RideKeys.event] = self.eventId
+        map[RideKeys.gcm_id] = self.gcmId
+        map[RideKeys.gender] = self.gender
+        map[RideKeys.radius] = self.radius
+        map[RideKeys.seats] = self.seats
+        map[RideKeys.v] = 0
+        map[RideKeys.time] = self.time
+        map[RideKeys.location] = self.getLocationAsDict()
+        map[RideKeys.passengers] = self.passengers
+
+        return map
+    }
+    
+    func getLocationAsDict()->[String:AnyObject]{
+        var map = [String:AnyObject]()
+        var locMap = [String:String]()
+        
+        locMap[LocationKeys.postcode] = self.postcode
+        locMap[LocationKeys.state] = self.state
+        locMap[LocationKeys.street1] = self.street
+        locMap[LocationKeys.country] = self.country
+        locMap[LocationKeys.suburb] = self.suburb
+        
+        map[LocationKeys.loc] = locMap
+        
+        return map
     }
 }
 
