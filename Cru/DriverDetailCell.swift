d4ed395a66aa3f8c2d3618210ebefa7b42c4fd84
maxcrane <Max@LC3.att.net>
2016-05-19 18:38:34 -0700

    improve the user interface of the driver ride detail page

---
+++ Cru/DriverDetailCell.swift
@@ -0,0 +1,14 @@
+//
+//  DriverDetailCell.swift
+//  Cru
+//
+//  Created by Max Crane on 5/19/16.
+//  Copyright © 2016 Jamaican Hopscotch Mafia. All rights reserved.
+//
+
+import UIKit
+
+class DriverDetailCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var value: UITextView!
+}
