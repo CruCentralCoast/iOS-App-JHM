874a31a96df4dcdf928402f94dbffb9fe1fdac76
maxcrane <Max@LC3.att.net>
2016-05-30 23:09:51 -0700

    applied the expose static dependency breaking technique to break up large phone number and name editing functions out of two view controllers into global utils

--- Cru/GlobalUtils.swift
+++ Cru/GlobalUtils.swift
@@ -10,6 +10,13 @@ import UIKit
 import ImageLoader
 
 class GlobalUtils {
+    
+    class PhoneRes {
+        var shouldChange = false
+        var newText = ""
+    }
+    
+    
     class func stringFromLocation(location: NSDictionary?) -> String {
         var locStr = ""
         
@@ -140,5 +147,58 @@ class GlobalUtils {
         let finalPhoneNum = areaCode + threeDigits + right
         return finalPhoneNum
         
+    }
+    
+    static func shouldChangeNameTextInRange(textView: UITextView, range: NSRange, text: String)->Bool{
+        let currentCharacterCount = textView.text?.characters.count ?? 0
+        if (range.length + range.location > currentCharacterCount){
+            return false
+        }
+        let newLength = currentCharacterCount + text.characters.count - range.length
+        return newLength <= 50
+    }
+    
+    
+    static func shouldChangePhoneTextInRange(num : String, range: NSRange, replacementText: String)->PhoneRes{
+        let res = PhoneRes()
+        res.newText = num
+        
+        let newString = (num as NSString).stringByReplacingCharactersInRange(range, withString: replacementText)
+        let components = newString.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet)
+        
+        let decimalString = components.joinWithSeparator("") as NSString
+        let length = decimalString.length
+        let hasLeadingOne = length > 0 && decimalString.characterAtIndex(0) == (1 as unichar)
+        
+        if length == 0 || (length > 10 && !hasLeadingOne) || length > 11 {
+            let newLength = (num as NSString).length + (replacementText as NSString).length - range.length as Int
+            res.shouldChange = (newLength > 10) ? false : true
+            return res
+        }
+        var index = 0 as Int
+        let formattedString = NSMutableString()
+        
+        if hasLeadingOne {
+            formattedString.appendString("1 ")
+            index += 1
+        }
+        if (length - index) > 3 {
+            let areaCode = decimalString.substringWithRange(NSMakeRange(index, 3))
+            formattedString.appendFormat("(%@) ", areaCode)
+            index += 3
+        }
+        if length - index > 3 {
+            let prefix = decimalString.substringWithRange(NSMakeRange(index, 3))
+            formattedString.appendFormat("%@-", prefix)
+            index += 3
+        }
+        
+        let remainder = decimalString.substringFromIndex(index)
+        formattedString.appendString(remainder)
+        
+        res.newText = formattedString as String
+        res.shouldChange = false
+        
+        return res
     }
 }
